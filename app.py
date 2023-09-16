e625f0ea191f6adfec8cc232539e84f662de5500
LauVertical <Vertical-driver@hotmail.com>
2023-09-16 23:54:25 +0800

    update
    Signed-off-by: LauVertical <Vertical-driver@hotmail.com>

--- app.py
+++ app.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
-import tornado
+
+import json
+import traceback
 import argparse
+
+from flask import Flask, request, jsonify, make_response
+from webservice.apiList import apiList
 
 
 def parse_arguments():
@@ -14,17 +19,88 @@ def parse_arguments():
 
 if __name__ == "__main__":
 
-    from webservice.requestHander import RequestHandler
-    from webservice.apiListHander import ApiListHandler
+    app = Flask(__name__)
+
+
+    @app.after_request
+    def add_cors_headers(response):
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        return response
+
+
+    @app.route('/query', methods=['POST', 'OPTIONS'])
+    def query():
+        if request.method == 'OPTIONS':
+            res = make_response()
+            res.status_code = 204
+            return res
+        else:
+            try:
+                data = request.json
+                if not data:
+                    return jsonify({
+                        'status': 400,
+                        'message': 'Cannot request from null query'
+                    })
+                api_name = data.get("api_name")
+                if not api_name:
+                    return jsonify({
+                        'status': 500,
+                        'message': traceback.format_exc()
+                    })
+
+                api = apiList.get(api_name)
+                if not api:
+                    return jsonify({
+                        'status': 500,
+                        'message': f'API {api_name} not found'
+                    })
+
+                req = {}
+                if "query" in data:
+                    req["query"] = data["query"]
+                if "queryKW" in data:
+                    req["queryKW"] = data["queryKW"]
+
+                content = api.request(req)
+
+                return content
+
+            except Exception:
+                return jsonify({
+                    'status': 500,
+                    'message': traceback.format_exc()
+                })
+
+
+    @app.route('/apilist', methods=['GET', 'OPTIONS'])
+    def api_list():
+        if request.method == 'OPTIONS':
+            res = make_response()
+            res.status_code = 204
+            return res
+        else:
+            try:
+                data = {"apilist": []}
+
+                for key in apiList.keys():
+                    data["apilist"].append(key)
+
+                response_str = json.dumps(data, ensure_ascii=False)
+
+                return response_str
+
+            except Exception:
+                response = {
+                    'status': 500,
+                    'message': traceback.format_exc()
+                }
+
+                return jsonify(response)
+
 
     args = parse_arguments()
 
-    handlers = [
-        (r'/query', RequestHandler),
-        (r'/apilist', ApiListHandler),
-    ]
-
-    app = tornado.web.Application(handlers=handlers)
-    # http_server = tornado.httpserver.HTTPServer(app)
-    app.listen(args.port)
-    tornado.ioloop.IOLoop.instance().start()
+    app.run(port=args.port)
